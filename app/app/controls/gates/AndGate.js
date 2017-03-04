5f551d2c5f2bd99640944be47fd5b012936bdcbc
Adil Aliyev <adilaliev@gmail.com>
2017-03-04 19:32:50 +0900

    [AndGate] Make the path coordinats values integer
    All values for creating an SVG path are now integers.
    
    Signed-off-by: Adil Aliyev <adilaliev@gmail.com>

--- app/app/controls/gates/AndGate.js
+++ app/app/controls/gates/AndGate.js
@@ -30,9 +30,9 @@ class AndGate extends BaseControl {
     initControl() {
         super.initControl();
 
-        let componentBody = this.paper.path("m 144.5892,3.7499993 0,158.5000007 71.29883,0 c 39.3576,0 71.30079,-35.50398 71.30079,-79.250001 0,-43.746016 -31.94225,-79.2500247 -70.83985,-79.2499997 l -71.75977,0 z M 28.677094,22.999999 l 112.999996,0 -112.999996,0 z m 260.210936,60 113,0 -113,0 z M 28.677094,143 141.67709,143 28.677094,143 Z");
-        let componentPin1 = new ConnectionPin(this, 15, 23.000004, "in");
-        let componentPin2 = new ConnectionPin(this, 15, 143.00003, "in");
+        let componentBody = this.paper.path("m 144,3 0,158 71,0 c 39,0 71,-35 71,-79 0,-43 -31,-79 -70,-79 l -71,0 z M 28,22 l 112,0 -112,0 z m 260,60 113,0 -113,0 z M 28,143 141,143 28,143 Z");
+        let componentPin1 = new ConnectionPin(this, 15, 23, "in");
+        let componentPin2 = new ConnectionPin(this, 15, 143, "in");
         let componentPin3 = new ConnectionPin(this, 415, 83, "out");
         let _this = this;
         componentPin1.addStateChangeListener(
