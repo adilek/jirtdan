38592bfd3baea6dabdcb35c3cf5fc15bef6fc9b2
Adil Aliyev <adilaliev@gmail.com>
2017-03-08 15:10:00 +0900

    [OrGate] Minor refactoring
    Signed-off-by: Adil Aliyev <adilaliev@gmail.com>

--- app/app/controls/gates/OrGate.js
+++ app/app/controls/gates/OrGate.js
@@ -1,6 +1,16 @@
 "use strict";
 /*jshint esversion: 6*/
 
+/**
+ * Control for OR gate.
+ *
+ * Truth table:
+ * |In1|In2|Out|
+ * |0  |0  |0  |
+ * |0  |1  |1  |
+ * |1  |0  |1  |
+ * |1  |1  |1  |
+ */
 class OrGate extends BaseControl {
     /**
      * Constructor for AndGate
@@ -16,29 +26,27 @@ class OrGate extends BaseControl {
         super.initControl();
 
         let componentBody = this.paper.path("m 101.5,3.5 7,8 7,8 c 0,0 20,24 20,60 0,36 -20,60 -20,60 l -15,17 24,0 53,0 c 9,0 30,0 54,-9 23,-9 50,-28 70,-64 1,-2 2,-4 2,-5 l -2,-4 c -39,-69 -105,-71 -123,-71 l -53,0 z m 203,75 99,0 m -274.48844,41 -99.51156,0 m 99,-73 -99,0");
-        let componentPin1 = new ConnectionPin(this, 15, 47, "in");
-        let componentPin2 = new ConnectionPin(this, 15, 119, "in");
-        let componentPin3 = new ConnectionPin(this, 415, 78, "out");
+        let inputPin1 = new ConnectionPin(this, 15, 47, "in");
+        let inputPin2 = new ConnectionPin(this, 15, 119, "in");
+        let outputPin3 = new ConnectionPin(this, 415, 78, "out");
         let _this = this;
-        componentPin1.addStateChangeListener(
+        inputPin1.addStateChangeListener(
             function (newState) {
                 _this.pin1Value = newState;
-                componentPin3.notifyStateChange(_this.getValue());
+                outputPin3.notifyStateChange(_this.getValue());
 
             });
 
-        componentPin2.addStateChangeListener(
+        inputPin2.addStateChangeListener(
             function (newState) {
                 _this.pin2Value = newState;
-                componentPin3.notifyStateChange(_this.getValue());
+                outputPin3.notifyStateChange(_this.getValue());
             });
 
         this.setShapes([componentBody]);
-        this.addInputPins(componentPin1, componentPin2);
-        this.addOutputPins(componentPin3);
+        this.addInputPins(inputPin1, inputPin2);
+        this.addOutputPins(outputPin3);
     }
-
-
 
     getValue() {
         return this.pin1Value || this.pin2Value;
