2cc4b0027a284550b04bbe05c2322deeb6392d93
Adil Aliyev <adilaliev@gmail.com>
2017-03-08 15:11:14 +0900

    [AndGate] Minor refactoring
    Signed-off-by: Adil Aliyev <adilaliev@gmail.com>

--- app/app/controls/gates/AndGate.js
+++ app/app/controls/gates/AndGate.js
@@ -31,29 +31,27 @@ class AndGate extends BaseControl {
         super.initControl();
 
         let componentBody = this.paper.path("m 144,3 0,158 71,0 c 39,0 71,-35 71,-79 0,-43 -31,-79 -70,-79 l -71,0 z M 28,22 l 112,0 -112,0 z m 260,60 113,0 -113,0 z M 28,143 141,143 28,143 Z");
-        let componentPin1 = new ConnectionPin(this, 15, 23, "in");
-        let componentPin2 = new ConnectionPin(this, 15, 143, "in");
-        let componentPin3 = new ConnectionPin(this, 415, 83, "out");
+        let inputPin1 = new ConnectionPin(this, 15, 23, "in");
+        let inputPin2 = new ConnectionPin(this, 15, 143, "in");
+        let outputPin3 = new ConnectionPin(this, 415, 83, "out");
         let _this = this;
-        componentPin1.addStateChangeListener(
+        inputPin1.addStateChangeListener(
             function (newState) {
                 _this.pin1Value = newState;
-                componentPin3.notifyStateChange(_this.getValue());
+                outputPin3.notifyStateChange(_this.getValue());
 
             });
 
-        componentPin2.addStateChangeListener(
+        inputPin2.addStateChangeListener(
             function (newState) {
                 _this.pin2Value = newState;
-                componentPin3.notifyStateChange(_this.getValue());
+                outputPin3.notifyStateChange(_this.getValue());
             });
 
         this.setShapes([componentBody]);
-        this.addInputPins(componentPin1, componentPin2);
-        this.addOutputPins(componentPin3);
+        this.addInputPins(inputPin1, inputPin2);
+        this.addOutputPins(outputPin3);
     }
-
-
 
     getValue() {
         return this.pin1Value & this.pin2Value;
