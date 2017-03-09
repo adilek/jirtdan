f5a7a76c487cd34b92801cb4116116c2f92fc416
Adil Aliyev <adilaliev@gmail.com>
2017-03-09 21:53:33 +0900

    [Misc] Very little changes
    Signed-off-by: Adil Aliyev <adilaliev@gmail.com>

--- app/app/controls/terminals/HighConstant.js
+++ app/app/controls/terminals/HighConstant.js
@@ -23,10 +23,10 @@ class HighConstant extends BaseControl {
 
         let componentBody = this.paper.path("m 2,3 0,159 122,0 0,-159 -122,0 z M 126,83 l 49,0 -49,0 z");
         let componentText = this.paper.path("m 30,119 c 8,0 17,0 25,0 0,-24 0,-48 0,-72 -8,1 -17,3 -26,5 0,-6 0,-13 0,-19 8,-1 17,-3 26,-5 9,0 18,0 27,0 0,30 0,61 0,92 8,0 17,0 25,0 0,6 0,13 0,19 -26,0 -52,0 -78,0 0,-6 0,-13 0,-19 z");
-        var componentPin3 = new ConnectionPin(this, 186, 83, "out");
+        let outputPin3 = new ConnectionPin(this, 186, 83, "out");
 
         this.setShapes([componentBody, componentText]);
-        this.addOutputPins(componentPin3);
+        this.addOutputPins(outputPin3);
     }
 
     getValue() {
--- app/app/controls/terminals/LowConstant.js
+++ app/app/controls/terminals/LowConstant.js
@@ -23,10 +23,10 @@ class LowConstant extends BaseControl {
 
         let componentBody = this.paper.path("m 2,3 0,159 122,0 0,-159 -122,0 z M 126,83 l 49,0 -49,0 z");
         let componentText = this.paper.path("m 81,83 q 0,-20 -3,-28 -3,-8 -12,-8 -9,0 -12,8 -3,8 -3,28 0,20 3,29 3,8 12,8 8,0 12,-8 3,-8 3,-29 z m 28,0 q 0,27 -11,41 -11,14 -33,14 -21,0 -33,-14 -11,-14 -11,-41 0,-27 11,-41 Q 43,27 65,27 q 21,0 33,14 11,14 11,41 z");
-        let componentPin3 = new ConnectionPin(this, 186, 83, "out");
+        let outputPin3 = new ConnectionPin(this, 186, 83, "out");
 
         this.setShapes([componentBody, componentText]);
-        this.addOutputPins(componentPin3);
+        this.addOutputPins(outputPin3);
     }
 
     getValue() {
