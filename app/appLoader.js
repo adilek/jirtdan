71c427e3781f98e96fef4dbbd256c8cb50b199b7
Adil Aliyev <adilaliev@gmail.com>
2017-03-12 17:44:25 +0900

    [Misc] Move the loader codes into app.js
    Signed-off-by: Adil Aliyev <adilaliev@gmail.com>

--- app/appLoader.js
+++ app/appLoader.js
@@ -1,6 +1,13 @@
+import {Board} from './app/Board.js';
+import {AndGate} from './app/controls/gates/AndGate.js'
+import {OrGate} from 'app/controls/gates/OrGate.js'
+import {HighConstant} from 'app/controls/terminals/HighConstant.js'
+import {LowConstant} from 'app/controls/terminals/LowConstant.js'
+import {Bulb} from 'app/controls/terminals/Bulb.js'
+
+let board = new Board(Raphael("board", 1280, 800));
+
 $(document).ready(function () {
-
-
     $("#btn-add-and").click(function (event) {
         board.addControl(new AndGate(board));
     });
