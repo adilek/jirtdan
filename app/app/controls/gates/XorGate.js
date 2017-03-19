9cb7826df13452d987f755eb414f6990afd27551
Adil Aliyev <adilaliev@gmail.com>
2017-03-19 23:39:08 +0900

    [XorGate] Fix the pin coordinates
    Signed-off-by: Adil Aliyev <adilaliev@gmail.com>

--- app/app/controls/gates/XorGate.js
+++ app/app/controls/gates/XorGate.js
@@ -53,9 +53,9 @@ export class XorGate extends BaseControl {
         super.initControl();
 
         let componentBody = this.paper.path("m 118,4 2,3 0,0 c 0,0 24,29 24,74 0,44 -24,74 -24,74 l -0,0 -2,3 4,0 57,0 c 9,0 30,0 54,-9 23,-9 49,-29 70,-67 l 0,-1 -0,-1 C 263,4 199,4 180,4 l -57,0 -4,0 z m -23,2 1,1 0,0 0,0 0,0 c 0,0 7,9 13,24 0,1 0,2 1,3 0,1 0,2 1,3 l -77,0 0,4 78,0 c 3,11 6,24 6,39 0,14 -2,27 -5,38 l -79,0 0,4 77,0 c -0,2 -1,4 -2,6 -0,1 -0,2 -1,3 -6,14 -12,22 -13,22 l -0,0 -0,0 -0,0 -1,1 2,0 c 0,-0 0,-0 0,-0 1,-1 3,-4 6,-9 l 0,-0 -0,0 0,-0 0,-0 c 2,-4 5,-9 7,-15 0,-1 0,-2 1,-3 0,-1 1,-3 2,-5 l 18,0 0,-4 -16,0 c 3,-10 5,-23 5,-38 0,-15 -2,-28 -6,-39 l 17,0 0,-4 -18,0 c -0,-0 -0,-1 -0,-2 -0,-1 -0,-2 -1,-3 -2,-7 -6,-13 -8,-17 l -0,-0 0,-0 c -2,-3 -4,-6 -6,-9 l -2,0 z m 212,74 0,4 98,0 0,-4 -98,0 z");
-        let inputPin1 = new ConnectionPin(this, 15, 47, "in");
-        let inputPin2 = new ConnectionPin(this, 15, 119, "in");
-        let outputPin = new ConnectionPin(this, 415, 78, "out");
+        let inputPin1 = new ConnectionPin(this, 18, 40, "in");
+        let inputPin2 = new ConnectionPin(this, 18, 120, "in");
+        let outputPin = new ConnectionPin(this, 415, 81, "out");
         let _this = this;
         inputPin1.addStateChangeListener(
             function (newState) {
