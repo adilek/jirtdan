73e9ce7df057a763f44f5a83cc0627087862797e
Adil Aliyev <adilaliev@gmail.com>
2017-04-09 15:45:37 +0900

    [NotGate] SonarQube issues resolved
    Signed-off-by: Adil Aliyev <adilaliev@gmail.com>

--- app/app/controls/gates/NotGate.js
+++ app/app/controls/gates/NotGate.js
@@ -54,47 +54,47 @@ export class NotGate extends BaseControl {
     initControl() {
         super.initControl();
 
-        let componentBodyShape = this.paper.path("M 30.561946,1.9908772 76.707033,24.924195 30.561946,47.85626 l 0,-45.8653828 z");
+        const componentBodyShape = this.paper.path("M 30.561946,1.9908772 76.707033,24.924195 30.561946,47.85626 l 0,-45.8653828 z");
         componentBodyShape.attr("stroke", DEFAULT_STROKE_COLOR);
         componentBodyShape.attr("stroke-width", DEFAULT_STROKE_WIDTH);
         componentBodyShape.attr("stroke-linejoin", "round");
         componentBodyShape.attr("stroke-miterlimit", 4);
 
-        let componentBodyShapeFill = this.paper.path("M 30.561946,1.9908772 76.707033,24.924195 30.561946,47.85626 l 0,-45.8653828 z");
+        const componentBodyShapeFill = this.paper.path("M 30.561946,1.9908772 76.707033,24.924195 30.561946,47.85626 l 0,-45.8653828 z");
         componentBodyShapeFill.attr("stroke-width", 1);
         componentBodyShapeFill.attr("stroke", DEFAULT_FILL_COLOR);
         componentBodyShapeFill.attr("fill", DEFAULT_FILL_COLOR);
 
-        let componentBodyShapeGradient = this.paper.path("m 30.561946,1.9908772 0,19.4008788 c 1.331848,-0.311925 2.699954,-0.582136 4.135594,-0.762063 4.743957,-0.405697 9.71957,0.65715 12.652942,2.766593 2.963578,1.6889 5.421043,3.891324 9.56834,4.746042 3.580402,0.430283 7.326277,0.244459 10.943607,0.0328 0.883108,-0.08248 1.753554,-0.195534 2.62944,-0.295183 L 76.707963,24.791159 30.82957,1.9908772 c -0.08922,0 -0.178416,0 -0.267624,0 z");
+        const componentBodyShapeGradient = this.paper.path("m 30.561946,1.9908772 0,19.4008788 c 1.331848,-0.311925 2.699954,-0.582136 4.135594,-0.762063 4.743957,-0.405697 9.71957,0.65715 12.652942,2.766593 2.963578,1.6889 5.421043,3.891324 9.56834,4.746042 3.580402,0.430283 7.326277,0.244459 10.943607,0.0328 0.883108,-0.08248 1.753554,-0.195534 2.62944,-0.295183 L 76.707963,24.791159 30.82957,1.9908772 c -0.08922,0 -0.178416,0 -0.267624,0 z");
         componentBodyShapeGradient.attr("stroke-width", 0);
         componentBodyShapeGradient.attr("fill", "90-#0066ff-#fff");
 
-        let componentBodyShapeSmallCircle = this.paper.path("m 30.402093,0.99310903 0,48.01216997 L 78.706762,24.999194 30.402093,0.99310903 Z M 84.491132,19.448644 a 5.6206879,5.5509597 0 0 0 -5.620274,5.55055 5.6206879,5.5509597 0 0 0 5.620274,5.550552 5.6206879,5.5509597 0 0 0 5.620275,-5.550552 5.6206879,5.5509597 0 0 0 -5.620275,-5.55055 z");
+        const componentBodyShapeSmallCircle = this.paper.path("m 30.402093,0.99310903 0,48.01216997 L 78.706762,24.999194 30.402093,0.99310903 Z M 84.491132,19.448644 a 5.6206879,5.5509597 0 0 0 -5.620274,5.55055 5.6206879,5.5509597 0 0 0 5.620274,5.550552 5.6206879,5.5509597 0 0 0 5.620275,-5.550552 5.6206879,5.5509597 0 0 0 -5.620275,-5.55055 z");
         componentBodyShapeSmallCircle.attr("stroke", DEFAULT_STROKE_COLOR);
         componentBodyShapeSmallCircle.attr("stroke-width", DEFAULT_STROKE_WIDTH);
         componentBodyShapeSmallCircle.attr("stroke-linejoin", "round");
         componentBodyShapeSmallCircle.attr("stroke-miterlimit", 4);
 
-        let componentBodyShapeSmallCircleFill = this.paper.path("m 84.451234,20.446937 c 2.563397,1.77e-4 4.619882,2.03115 4.620072,4.562759 -1.9e-4,2.531597 -2.056675,4.56257 -4.620072,4.562757 -2.563397,-1.87e-4 -4.619882,-2.03116 -4.620072,-4.562757 1.9e-4,-2.531609 2.056675,-4.562582 4.620072,-4.562759 z");
+        const componentBodyShapeSmallCircleFill = this.paper.path("m 84.451234,20.446937 c 2.563397,1.77e-4 4.619882,2.03115 4.620072,4.562759 -1.9e-4,2.531597 -2.056675,4.56257 -4.620072,4.562757 -2.563397,-1.87e-4 -4.619882,-2.03116 -4.620072,-4.562757 1.9e-4,-2.531609 2.056675,-4.562582 4.620072,-4.562759 z");
         componentBodyShapeSmallCircleFill.attr("stroke-width", 1);
         componentBodyShapeSmallCircleFill.attr("stroke", DEFAULT_FILL_COLOR);
         componentBodyShapeSmallCircleFill.attr("fill", DEFAULT_FILL_COLOR);
 
-        let componentBodyShapeSmallCircleGradient = this.paper.path("m 84.451234,20.446937 c -2.126299,1.48e-4 -3.884153,1.406608 -4.428633,3.328017 3.390024,0.192324 6.602592,1.0941 8.835772,2.538936 0.125065,-0.415574 0.212893,-0.846934 0.212933,-1.304194 -1.9e-4,-2.531609 -2.056675,-4.562582 -4.620072,-4.562759 z");
+        const componentBodyShapeSmallCircleGradient = this.paper.path("m 84.451234,20.446937 c -2.126299,1.48e-4 -3.884153,1.406608 -4.428633,3.328017 3.390024,0.192324 6.602592,1.0941 8.835772,2.538936 0.125065,-0.415574 0.212893,-0.846934 0.212933,-1.304194 -1.9e-4,-2.531609 -2.056675,-4.562582 -4.620072,-4.562759 z");
         componentBodyShapeSmallCircleGradient.attr("stroke-width", 0);
         componentBodyShapeSmallCircleGradient.attr("fill", "90-#0066ff-#fff");
 
-        let componentOutputWire = this.paper.path("m 90.907772,24.999597 19.554188,0");
+        const componentOutputWire = this.paper.path("m 90.907772,24.999597 19.554188,0");
         componentOutputWire.attr("stroke", DEFAULT_STROKE_COLOR);
         componentOutputWire.attr("stroke-width", DEFAULT_STROKE_WIDTH);
 
-        let componentInputWire = this.paper.path("m 10.907768,24.999597 19.554188,0");
+        const componentInputWire = this.paper.path("m 10.907768,24.999597 19.554188,0");
         componentInputWire.attr("stroke", DEFAULT_STROKE_COLOR);
         componentInputWire.attr("stroke-width", DEFAULT_STROKE_WIDTH);
 
-        let inputPin = new ConnectionPin(this, 5, 25, "in");
-        let outputPin = new ConnectionPin(this, 116, 25, "out");
-        let _this = this;
+        const inputPin = new ConnectionPin(this, 5, 25, "in");
+        const outputPin = new ConnectionPin(this, 116, 25, "out");
+        const _this = this;
 
         inputPin.addStateChangeListener(
             function (newState) {
