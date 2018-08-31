29e8ea0d314531e56c290dceba7d4399d9092a05
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2018-08-31 21:32:03 +0400

    configured bundle js

--- app/appLoader.js
+++ app/appLoader.js
@@ -22,81 +22,81 @@
 
  */
 
-import {Board} from './app/Board.js';
-import {AndGate} from './app/controls/gates/AndGate.js'
-import {OrGate} from 'app/controls/gates/OrGate.js'
-import {HighConstant} from 'app/controls/terminals/HighConstant.js'
-import {LowConstant} from 'app/controls/terminals/LowConstant.js'
-import {Bulb} from 'app/controls/terminals/Bulb.js'
-import {NotGate} from 'app/controls/gates/NotGate.js'
-import {BufferGate} from 'app/controls/gates/BufferGate.js'
-import {XorGate} from 'app/controls/gates/XorGate.js'
-import {SwitchButton} from 'app/controls/terminals/SwitchButton.js'
-import {PushButton} from 'app/controls/terminals/PushButton.js'
-import {NandGate} from './app/controls/gates/NandGate.js'
-import {NorGate} from './app/controls/gates/NorGate.js'
-import {XnorGate} from './app/controls/gates/XnorGate.js'
+import { Board } from './app/Board.js';
+import { AndGate } from './app/controls/gates/AndGate.js';
+import { OrGate } from './app/controls/gates/OrGate.js';
+import { HighConstant } from './app/controls/terminals/HighConstant.js';
+import { LowConstant } from './app/controls/terminals/LowConstant.js';
+import { Bulb } from './app/controls/terminals/Bulb.js';
+import { NotGate } from './app/controls/gates/NotGate.js';
+import { BufferGate } from './app/controls/gates/BufferGate.js';
+import { XorGate } from './app/controls/gates/XorGate.js';
+import { SwitchButton } from './app/controls/terminals/SwitchButton.js';
+import { PushButton } from './app/controls/terminals/PushButton.js';
+import { NandGate } from './app/controls/gates/NandGate.js';
+import { NorGate } from './app/controls/gates/NorGate.js';
+import { XnorGate } from './app/controls/gates/XnorGate.js';
 
-let board = new Board(Raphael("board", 1280, 800));
+let board = new Board(Raphael('board', 1280, 800));
 
+$(document).ready(function() {
+	$('#board')
+		.mousedown(function(event) {
+			'use strict';
+			board.onMouseDown(event);
+		})
+		.mousemove(function(event) {
+			'use strict';
+			board.onMouseMove(event);
+		})
+		.mouseup(function(event) {
+			'use strict';
+			board.onMouseUp(event);
+		});
 
-$(document).ready(function () {
-
-    $("#board").mousedown(function (event) {
-        "use strict";
-        board.onMouseDown(event)
-    }).mousemove(function (event) {
-        "use strict";
-        board.onMouseMove(event)
-    }).mouseup(function (event) {
-        "use strict";
-        board.onMouseUp(event)
-    });
-
-
-    $("#btn-add-and").click(function (event) {
-        board.addControl(new AndGate(board));
-    });
-    $("#btn-add-one").click(function (event) {
-        board.addControl(new HighConstant(board));
-    });
-    $("#btn-add-zero").click(function (event) {
-        board.addControl(new LowConstant(board));
-    });
-    $("#btn-add-bulb").click(function (event) {
-        board.addControl(new Bulb(board));
-    });
-    $("#btn-add-or").click(function (event) {
-        board.addControl(new OrGate(board));
-    });
-    $("#btn-add-not").click(function (event) {
-        board.addControl(new NotGate(board));
-    });
-    $("#btn-add-xor").click(function (event) {
-        board.addControl(new XorGate(board));
-    });
-    $("#btn-add-switch").click(function (event) {
-        board.addControl(new SwitchButton(board));
-    });
-    $("#btn-add-push").click(function (event) {
-        board.addControl(new PushButton(board));
-    });
-    $("#btn-add-buffer").click(function (event) {
-        board.addControl(new BufferGate(board));
-    });
-    $("#btn-add-nand").click(function (event) {
-        board.addControl(new NandGate(board));
-    });
-    $("#btn-add-nor").click(function (event) {
-        board.addControl(new NorGate(board));
-    });
-    $("#btn-add-xnor").click(function (event) {
-        board.addControl(new XnorGate(board));
-    });
-    $("#btn-delete").click(function (event) {
-        board.deleteSelected();
-    });
-    $("#btn-unselect").click(function (event) {
-        board.unselect();
-    });
+	$('#btn-add-and').click(function(event) {
+		board.addControl(new AndGate(board));
+	});
+	$('#btn-add-one').click(function(event) {
+		board.addControl(new HighConstant(board));
+	});
+	$('#btn-add-zero').click(function(event) {
+		board.addControl(new LowConstant(board));
+	});
+	$('#btn-add-bulb').click(function(event) {
+		board.addControl(new Bulb(board));
+	});
+	$('#btn-add-or').click(function(event) {
+		board.addControl(new OrGate(board));
+	});
+	$('#btn-add-not').click(function(event) {
+		board.addControl(new NotGate(board));
+	});
+	$('#btn-add-xor').click(function(event) {
+		board.addControl(new XorGate(board));
+	});
+	$('#btn-add-switch').click(function(event) {
+		board.addControl(new SwitchButton(board));
+	});
+	$('#btn-add-push').click(function(event) {
+		board.addControl(new PushButton(board));
+	});
+	$('#btn-add-buffer').click(function(event) {
+		board.addControl(new BufferGate(board));
+	});
+	$('#btn-add-nand').click(function(event) {
+		board.addControl(new NandGate(board));
+	});
+	$('#btn-add-nor').click(function(event) {
+		board.addControl(new NorGate(board));
+	});
+	$('#btn-add-xnor').click(function(event) {
+		board.addControl(new XnorGate(board));
+	});
+	$('#btn-delete').click(function(event) {
+		board.deleteSelected();
+	});
+	$('#btn-unselect').click(function(event) {
+		board.unselect();
+	});
 });
