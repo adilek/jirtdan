eaffed04d92abe27c35f2d5828d0024d0456601d
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2018-08-31 21:27:39 +0400

    changed webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './app/appLoader.js',
-  output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
-  plugins: [new UglifyJsPlugin()]
+	entry: './app/appLoader.js',
+	output: {
+		path: path.resolve(__dirname, 'app/compiled'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [ '@babel/preset-env' ]
+					}
+				}
+			}
+		]
+	},
+	plugins: [ new UglifyJsPlugin() ]
 };
